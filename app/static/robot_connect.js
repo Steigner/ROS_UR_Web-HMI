3cc91d48d3a041d380fb15e021c02e4f4bd1d4b4
Steigner <200543@seznam.cz>
2022-12-17 22:51:22 +0100

    tested

--- app/static/robot_connect.js
+++ app/static/robot_connect.js
@@ -99,3 +99,15 @@ $('#connect').click(function () {
         });
     });
 });
+
+window.onbeforeunload = function (e) {
+    e = e || window.event;
+
+    // For IE and Firefox prior to version 4
+    if (e) {
+        e.returnValue = 'Sure?';
+    }
+
+    // For Safari
+    return 'Sure?';
+};
--- app/static/robot_control.js
+++ app/static/robot_control.js
@@ -322,3 +322,15 @@ window.oncontextmenu = function (event) {
 };
 
 });
+
+window.onbeforeunload = function (e) {
+    e = e || window.event;
+
+    // For IE and Firefox prior to version 4
+    if (e) {
+        e.returnValue = 'Sure?';
+    }
+
+    // For Safari
+    return 'Sure?';
+};
